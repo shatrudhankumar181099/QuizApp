ec5860582f40550bc1b8ea56837c80395fae078a
shatrudhan kumar <shatrudhankumar181099@gmail.com>
2023-04-13 19:42:47 +0530

    Add Forget Password Feature

--- app/src/main/java/com/example/quizapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/quizapp/activities/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.example.quizapp.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.quizapp.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
-
+val auth = FirebaseAuth.getInstance()
 class LoginActivity : AppCompatActivity() {
 
     lateinit var firebaseAuth: FirebaseAuth
@@ -25,6 +29,36 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        btn_forget_password.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Forget Password")
+            val view = layoutInflater.inflate(R.layout.dialogbox_forget_psword, null)
+            val username = view.findViewById<EditText>(R.id.et_username)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
+                forgetPassword(username)
+            })
+            builder.setNegativeButton("Close", DialogInterface.OnClickListener { _, _ -> })
+            builder.show()
+        }
+    }
+    private fun forgetPassword(username:EditText) {
+        if (username.text.toString().isEmpty()) {
+
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()) {
+            return
+        }
+
+        auth.sendPasswordResetEmail(username.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this,"Email Sent", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
     private fun login(){
         val email = etEmailAddress.text.toString()
